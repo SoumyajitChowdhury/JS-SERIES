8bb8e40ecff268d1ecd8354bd33599407ab24334
Soumyajit Chowdhury <116186474+SoumyajitChowdhury@users.noreply.github.com>
2023-09-08 03:25:06 +0000

    data types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code as newer version 
+
+// alert(3 + 3)  -- we are using node.js not broweser
+
+let name = "Soumya"     //string
+
+let age = 21            //number
+
+let isLoggedIn = true   //boolean  --> true/false
+
+//null --> stand alone value
+
+//undefined --> value is not assigned till now
+
+//symbol
+
+console.log(typeof "Soumya"); //string
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
