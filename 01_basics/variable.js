a477ca4295a9d877442a0165a0fdace6dd4d3794
Soumyajit Chowdhury <116186474+SoumyajitChowdhury@users.noreply.github.com>
2023-09-08 03:00:58 +0000

    variable done

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1445535
+let accountEmain = "soumya1235@abc"
+var accountPass = "1236"
+accountCity = "Kolkata"
+let accountState;
+// accountId = 2 // not allowed
+accountEmain = "abc@sc"
+accountPass = "54574"
+accountCity = "Noida"
+
+
+console.log(accountId);
+console.table([accountId,accountEmain,accountPass,accountCity,accountState])
