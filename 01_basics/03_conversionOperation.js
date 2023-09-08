5b6f356ec3eda9d507734bd2cbcdeccfee912222
Soumyajit Chowdhury <116186474+SoumyajitChowdhury@users.noreply.github.com>
2023-09-08 17:11:00 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//33 -->number
+//"33" -->number
+//"33abc" -->NaN
+//null -->0
+//undefined ==>NaN
+//true=>1  false==>0
+
+
+let isLoggedIn = "soumya"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+//for any  number without 0 its always give true i..e bool
+//"" ---> false
+//"Soumya" -->true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
